250714953664da726d670e547dfa6c0877a09716
prabhav bhattu <prabhavbhattu@gmail.com>
2022-05-13 22:03:15 -0700

    Registered global warming event to main class

--- src/main/java/com/mercator/environmentalmechanics/EnvironmentalMechanics.java
+++ src/main/java/com/mercator/environmentalmechanics/EnvironmentalMechanics.java
@@ -1,12 +1,14 @@
 package com.mercator.environmentalmechanics;
 
+import com.mercator.environmentalmechanics.climate.GlobalWarming;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnvironmentalMechanics extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        GlobalWarming globalWarming = new GlobalWarming();
+        getServer().getPluginManager().registerEvents(globalWarming, this);
     }
 
     @Override
