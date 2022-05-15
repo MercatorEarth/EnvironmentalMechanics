f5bf53ff659f0bd2661632c0a3c87ddbf1dfe6f2
prabhav bhattu <prabhavbhattu@gmail.com>
2022-05-14 18:54:20 -0700

    Registered events and commands

--- src/main/java/com/mercator/environmentalmechanics/EnvironmentalMechanics.java
+++ src/main/java/com/mercator/environmentalmechanics/EnvironmentalMechanics.java
@@ -1,7 +1,9 @@
 package com.mercator.environmentalmechanics;
 
 import com.mercator.environmentalmechanics.climate.CarbonDioxideEvent;
+import com.mercator.environmentalmechanics.climate.MethaneEvent;
 import com.mercator.environmentalmechanics.climate.commands.CommandGetCarbonDioxide;
+import com.mercator.environmentalmechanics.climate.commands.CommandGetMethane;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnvironmentalMechanics extends JavaPlugin {
@@ -9,8 +11,13 @@ public final class EnvironmentalMechanics extends JavaPlugin {
     @Override
     public void onEnable() {
         CarbonDioxideEvent carbonDioxideEvent = new CarbonDioxideEvent();
+        MethaneEvent methaneEvent = new MethaneEvent();
+
         getServer().getPluginManager().registerEvents(carbonDioxideEvent, this);
+        getServer().getPluginManager().registerEvents(methaneEvent, this);
+
         this.getCommand("getco2").setExecutor(new CommandGetCarbonDioxide());
+        this.getCommand("getch4").setExecutor(new CommandGetMethane());
 
         getServer().getPluginManager().getPlugin("EnvironmentalMechanics").getLogger().info("Environmental Mechanics enabled successfully!");
     }
--- src/main/java/com/mercator/environmentalmechanics/climate/MethaneEvent.java
+++ src/main/java/com/mercator/environmentalmechanics/climate/MethaneEvent.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Block;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
@@ -13,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MethaneEvent {
+public class MethaneEvent implements Listener {
 
     public double methaneConcentration;
     private Map<String, Double> methaneGenValues;
